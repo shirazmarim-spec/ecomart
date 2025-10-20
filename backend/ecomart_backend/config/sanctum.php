e37111d2f68d6855c63dd140d63f4facc7bb9695
Bushra <shirazmarim@gmail.com>
2025-10-21 01:53:30 +0300

    Fixed sanctum stateful domains and tested tokeauth

--- backend/ecomart_backend/config/sanctum.php
+++ backend/ecomart_backend/config/sanctum.php
@@ -14,14 +14,10 @@ return [
     | and production domains which access your API via a frontend SPA.
     |
     */
-
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
-    ))),
-
+    'stateful' => explode(',', env(
+    'SANCTUM_STATEFUL_DOMAINS',
+    'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,localhost:8000'
+)),
     /*
     |--------------------------------------------------------------------------
     | Sanctum Guards
